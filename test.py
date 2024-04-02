3d6dee358c2e2a282b28844a9fb73a062ddb1809
Aamirsid123 <116836873+Aamirsid123@users.noreply.github.com>
2024-04-02 21:13:33 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,55 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import time
+cap = cv2.VideoCapture(0)
+detector = HandDetector()
+classifier = Classifier("model/keras_model.h5", "model/labels.txt")
+imgSize = 300
+offset = 20
+counter = 0
+labels =["A", "B", "C"]
+folder = "Photo/C"
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
+        imgCropShape = imgCrop.shape
+
+
+        aspectRatio = h / w
+        if aspectRatio > 1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape =imgResize.shape
+            wGap = math.ceil((imgSize-wCal)/2)
+            imgWhite[0:imgResizeShape[0], wGap:wCal+wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite)
+            print(prediction, index)
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap,0:imgResizeShape[1]] = imgResize
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+    # key = cv2.waitKey(1)
+    # if key == ord('s'):
+    #     counter += 1
+    #     cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+    #     print(counter)
